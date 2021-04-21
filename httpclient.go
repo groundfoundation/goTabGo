d255ed25e82559d1a938b701b23ee86388263e94
Joshua Meekhof <jmeekhof@twotheleft.com>
2021-04-21 16:35:10 -0400

    Add content type
    This type will be used to indicate what content type we are either
    sending or receiving.

---
+++ httpclient.go
@@ -0,0 +1,40 @@
+package gotabgo
+
+import (
+	"io"
+	"net/http"
+)
+
+const (
+	TABLEAU_AUTH_HEADER = "X-Tableau-Auth"
+)
+
+type httpClient struct {
+	c          http.Client
+	authToken  string
+	acceptType string
+}
+
+func (c *httpClient) Get(url string) (resp *http.Response, err error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	return c.Do(req)
+}
+
+func (c *httpClient) Post(url, contentType string, body io.Reader) (resp *http.Response, err error) {
+	req, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", contentType)
+	return c.c.Do(req)
+}
+
+func (c *httpClient) Do(req *http.Request) (*http.Response, error) {
+	if c.authToken != "" {
+		req.Header.Add(TABLEAU_AUTH_HEADER, c.authToken)
+	}
+	return c.c.Do(req)
+}
