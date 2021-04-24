dab551e060fe3721af6147f8e1e9e51fb94423be
Joshua Meekhof <jmeekhof@twotheleft.com>
2021-04-24 09:51:49 -0400

    Add method to convert string to type

--- types.go
+++ types.go
@@ -1,5 +1,7 @@
 package gotabgo
 
+import "errors"
+
 type ContentType int
 
 const (
@@ -9,6 +11,17 @@ const (
 
 func (r ContentType) String() string {
 	return [...]string{"application/json", "application/xml"}[r]
+}
+
+func ContentTypeString(s string) (c ContentType, e error) {
+	var cTypeMap = map[string]ContentType{"application/json": Json, "application/xml": Xml}
+	var ok bool
+
+	if c, ok = cTypeMap[s]; ok {
+		return
+	}
+	e = errors.New("Content Type can't be converted")
+	return
 }
 
 type TabApi struct {
