8a1b1e37db653a25a5c60c176d27f766a9cc8ae2
Joshua Meekhof <jmeekhof@twotheleft.com>
2021-04-26 10:24:50 -0400

    Reformat struct annotations
    Purely visual change that adds spaces between json and xml annotations.
    Makes it a bit easier to see what's xml and json specific.

--- model/tsreponse.go
+++ model/tsreponse.go
@@ -4,11 +4,11 @@ import "encoding/xml"
 
 // TsResponse is the wrapper that Tableau Server wraps each response with
 type TsResponse struct {
-	XMLName     xml.Name    `json:"-" xml:"tsResponse"`
-	ServerInfo  ServerInfo  `json:"serverInfo" xml:"serverInfo"`
-	Credentials Credentials `json:"credentials" xml:"credentials"`
-	Error       ErrorType   `json:"error" xml:"error"`
-	Site        SiteType    `json:"site" xml:"site"`
+	XMLName     xml.Name    `json:"-"            xml:"tsResponse"`
+	ServerInfo  ServerInfo  `json:"serverInfo"   xml:"serverInfo"`
+	Credentials Credentials `json:"credentials"  xml:"credentials"`
+	Error       ErrorType   `json:"error"        xml:"error"`
+	Site        SiteType    `json:"site"         xml:"site"`
 }
 
 // ServerInfo contains information about product version and api version for the server
@@ -19,49 +19,49 @@ type ServerInfo struct {
 }
 
 type SiteType struct {
-	XMLName      xml.Name `json:"-" xml:"site"`
-	ID           string   `json:"id,omitempty" xml:"id,attr,omitempty"`
-	Name         string   `json:"name,omitempty" xml:"name,attr,omitempty"`
-	ContentUrl   string   `json:"contentUrl,omitempty" xml:"contentUrl,attr,omitempty"`
-	AdminMode    string   `json:"adminMode,omitempty" xml:"adminMode,attr,omitempty"`
-	UserQuota    string   `json:"userQuota,omitempty" xml:"userQuota,attr,omitempty"`
-	StorageQuota int      `json:"storageQuota,omitempty" xml:"storageQuota,attr,omitempty"`
-	State        string   `json:"state,omitempty" xml:"state,attr,omitempty"`
-	StatusReason string   `json:"statusReason,omitempty" xml:"statusReason,attr,omitempty"`
+	XMLName      xml.Name `json:"-"                       xml:"site"`
+	ID           string   `json:"id,omitempty"            xml:"id,attr,omitempty"`
+	Name         string   `json:"name,omitempty"          xml:"name,attr,omitempty"`
+	ContentUrl   string   `json:"contentUrl,omitempty"    xml:"contentUrl,attr,omitempty"`
+	AdminMode    string   `json:"adminMode,omitempty"     xml:"adminMode,attr,omitempty"`
+	UserQuota    string   `json:"userQuota,omitempty"     xml:"userQuota,attr,omitempty"`
+	StorageQuota int      `json:"storageQuota,omitempty"  xml:"storageQuota,attr,omitempty"`
+	State        string   `json:"state,omitempty"         xml:"state,attr,omitempty"`
+	StatusReason string   `json:"statusReason,omitempty"  xml:"statusReason,attr,omitempty"`
 	Usage        struct {
-		NumUsers     uint `json:"numUsers" xml:"numUsers,attr"`
-		NumCreators  uint `json:"numCreators,omitempty" xml:"numCreators,omitempty,attr"`
-		NumExplorers uint `json:"numExplorers,omitempty" xml:"numExplorers,omitempty,attr"`
-		NumViewers   uint `json:"numViewers,omitempty" xml:"numViewers,omitempty,attr"`
-		Storage      uint `json:"storage" xml:"storage,attr"`
-	} `json:"usage,omitempty" xml:"usage,omitempty"`
+		NumUsers     uint `json:"numUsers"                xml:"numUsers,attr"`
+		NumCreators  uint `json:"numCreators,omitempty"   xml:"numCreators,omitempty,attr"`
+		NumExplorers uint `json:"numExplorers,omitempty"  xml:"numExplorers,omitempty,attr"`
+		NumViewers   uint `json:"numViewers,omitempty"    xml:"numViewers,omitempty,attr"`
+		Storage      uint `json:"storage"                 xml:"storage,attr"`
+	} `json:"usage,omitempty"  xml:"usage,omitempty"`
 }
 
 type Credentials struct {
-	XMLName     xml.Name  `json:"-" xml:"credentials"`
-	Name        string    `json:"name,omitempty" xml:"name,attr,omitempty"`
-	Password    string    `json:"password,omitempty" xml:"password,attr,omitempty"`
-	Token       string    `json:"token,omitempty" xml:"token,attr,omitempty"`
-	Site        *SiteType `json:"site,omitempty" xml:"site,omitempty"`
-	Impersonate *User     `json:"user,omitempty" xml:"user,omitempty"`
+	XMLName     xml.Name  `json:"-"                   xml:"credentials"`
+	Name        string    `json:"name,omitempty"      xml:"name,attr,omitempty"`
+	Password    string    `json:"password,omitempty"  xml:"password,attr,omitempty"`
+	Token       string    `json:"token,omitempty"     xml:"token,attr,omitempty"`
+	Site        *SiteType `json:"site,omitempty"      xml:"site,omitempty"`
+	Impersonate *User     `json:"user,omitempty"      xml:"user,omitempty"`
 }
 type ProductVersion struct {
-	XMLName xml.Name `json:"-" xml:"productVersion"`
-	Value   string   `json:"value" xml:",chardata"`
-	Build   string   `json:"build" xml:"build,attr"`
+	XMLName xml.Name `json:"-"      xml:"productVersion"`
+	Value   string   `json:"value"  xml:",chardata"`
+	Build   string   `json:"build"  xml:"build,attr"`
 }
 
 type User struct {
-	XMLName  xml.Name `json:"-" xml:"user"`
-	ID       string   `json:"id,omitempty" xml:"id,attr,omitempty"`
-	Name     string   `json:"name,omitempty" xml:"name,attr,omitempty"`
-	SiteRole string   `json:"siteRole,omitempty" xml:"siteRole,attr,omitempty"`
-	FullName string   `json:"fullName,omitempty" xml:"fullName,attr,omitempty"`
+	XMLName  xml.Name `json:"-"                   xml:"user"`
+	ID       string   `json:"id,omitempty"        xml:"id,attr,omitempty"`
+	Name     string   `json:"name,omitempty"      xml:"name,attr,omitempty"`
+	SiteRole string   `json:"siteRole,omitempty"  xml:"siteRole,attr,omitempty"`
+	FullName string   `json:"fullName,omitempty"  xml:"fullName,attr,omitempty"`
 }
 
 type ErrorType struct {
-	XMLName xml.Name `json:"-" xml:"error"`
-	Summary string   `json:"summary" xml:"summary"`
-	Detail  string   `json:"detail" xml:"detail"`
-	Code    uint     `json:"code" xml:"code,attr"`
+	XMLName xml.Name `json:"-"        xml:"error"`
+	Summary string   `json:"summary"  xml:"summary"`
+	Detail  string   `json:"detail"   xml:"detail"`
+	Code    uint     `json:"code"     xml:"code,attr"`
 }
