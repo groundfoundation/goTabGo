f95c00f0ae790644e77b86f3c5c520464780f277
Joshua Meekhof <jmeekhof@twotheleft.com>
2021-04-26 10:24:50 -0400

    Reformat struct annotations
    Purely visual change that adds spaces between json and xml annotations.
    Makes it a bit easier to see what's xml and json specific.

--- model/tsrequest.go
+++ model/tsrequest.go
@@ -4,9 +4,9 @@ import "encoding/xml"
 
 // TsRequest is the wrapper that Tableau Server expects requests to be wrapped with
 type TsRequest struct {
-	XMLName     xml.Name    `json:"-" xml:"tsRequest"`
-	Credentials Credentials `json:"credentials,omitempty" xml:"credentials,omitempty"`
-	Site        SiteType    `json:"site,omitempty" xml:"site,omitempty"`
+	XMLName     xml.Name    `json:"-"                      xml:"tsRequest"`
+	Credentials Credentials `json:"credentials,omitempty"  xml:"credentials,omitempty"`
+	Site        SiteType    `json:"site,omitempty"         xml:"site,omitempty"`
 }
 
 //
